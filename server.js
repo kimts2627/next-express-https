a39dfaae57a4d8b285e7061be09d3f0bc393e360
kimts2627 <kimtstststs@gmail.com>
2021-05-03 23:03:25 +0900

    create-next-app

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.prepare().then(() => {
 
   server.listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log(`> Ready on https://localhost:${port}`)
   })
 })
